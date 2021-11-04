455e9cbd8d9f262d101f4c28e335516e7849fd43
Melvin <monmail@gmail.com>
2021-11-04 16:27:43 +0100

    Route

--- src/Controller/Front/ProductController.php
+++ src/Controller/Front/ProductController.php
@@ -10,22 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProductController extends AbstractController
 {
     /**
-     * @Route("products", name"products_list")
+     * @Route("products", name="products_list")
      */
 
     public function products(ProductRepository $productRepository)
     {
         $products = $productRepository->findAll();
         return $this->render('front/product/products.html.twig', ['products' => $products]);
-    }
-
-    /**
-     * @Route("product/{id}", name="product")
-     */
-
-    public function product($id, ProductRepository $productRepository)
-    {
-        $product = $productRepository->find($id);
-        return $this->render('front/product/product.html.twig', ['product' => $product]);
     }
 }
